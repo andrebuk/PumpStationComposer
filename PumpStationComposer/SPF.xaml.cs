cb0eab29a9fd2790cec6d94a26e377f08d44adbb
Andrey Bukhvalov <andrey.bukhvalov@severminerals.com>
2021-04-23 19:02:47 +0300

    Заработало назначение параметров

--- PumpStationComposer/SPF.xaml.cs
+++ PumpStationComposer/SPF.xaml.cs
@@ -26,34 +26,78 @@ namespace CommonCommands
     {
         public Document document;
         public UIDocument uiDocument;
+        public DefinitionFile defFile;
+        public DefinitionGroups allDefGroups;
+        public Definitions definitions;
         public SPF(Document doc, UIDocument uidoc)
         {
             document = doc;
             uiDocument = uidoc;
-
+            defFile = uiDocument.Application.Application.OpenSharedParameterFile();
+            allDefGroups = defFile.Groups;
+            
             InitializeComponent();
         }
         public void setGroups()
         {
-
-
             //FamilyManager familyManager = document.FamilyManager;
+            // var fileName = uiDocument.Application.Application.SharedParametersFilename;
 
-            var fileName = uiDocument.Application.Application.SharedParametersFilename;
-            DefinitionFile defFile = uiDocument.Application.Application.OpenSharedParameterFile();
-
-            DefinitionGroups allDefGroups = defFile.Groups;
             foreach (var currentDefGroup in allDefGroups)
             {
                 this.Groups.Items.Add(currentDefGroup.Name);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FamilyManager familyManager = document.FamilyManager;
+            System.Collections.IList selectedParameters = this.Parameters.SelectedItems;
+            //DefinitionGroup myGroup = allDefGroups.get_Item(this.Groups.SelectedItem.ToString());
+            //definitions = myGroup.Definitions;
+            foreach (object currentParameter in selectedParameters)
+            {
+                ExternalDefinition eDef = definitions.get_Item(currentParameter.ToString()) as ExternalDefinition;
+                using (Transaction t = new Transaction(document, "Назначение общего параметра"))
+                {
+                    t.Start();
+                    try
+                    {
+                        familyManager.AddParameter(eDef, BuiltInParameterGroup.PG_GENERAL, this.paraInst.IsChecked==true);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                    t.Commit();
+
+                }
+
+            }
+
+            TaskDialog.Show("Revit message", "df");
+        }
+
+        private void Groups_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.Parameters.Items.Clear();
+            DefinitionGroup myGroup = allDefGroups.get_Item(this.Groups.SelectedItem.ToString());
+            definitions = myGroup.Definitions;
+            foreach (Definition item in definitions)
+            {
+                this.Parameters.Items.Add(item.Name);
             }
 
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void paraType_Checked(object sender, RoutedEventArgs e)
         {
-            TaskDialog.Show("Revit message", "Done!");
+            this.paraInst.IsChecked = false;
+        }
+
+        private void paraInst_Checked(object sender, RoutedEventArgs e)
+        {
+            this.paraType.IsChecked = false;
         }
     }
 }
